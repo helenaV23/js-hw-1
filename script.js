57a9885fb39d0a5baa00d934d052141aeab7f952
olena <vasechko.elenaa@gmail.com>
2022-06-01 12:00:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let s = 314.1592653589793;
+let r = 10;
+let h = 20;
+
+let v = Math.PI * r**2 * h;
+
+console.log("**************");
+console.log ("Обьем цилиндра с площадью основы S =", s, "радиусом R =", r, "и высотой H =", h, "равен:");
+console.log ("--------------------");
+console.log ("V =", v);
+console.log ("--------------------");
+console.log ("end");
